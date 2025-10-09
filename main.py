ae9e3564f1c881397888b8828173529f3fc36b61
Grace Conroy <fgc001@lvc.edu>
2025-10-09 10:46:38 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# An appointment is a dictionary with keys such as start time, end time, location, attendees, meeting purpose, title. 
+# Values for start and end time are hours and minutes. Location has values of a specific building, room number, address. 
+# Attendees values are names of people who will be at the appointment. 
+# Meeting purpose is the reason for attendance
+# Title is what the entire dictionary is assigned to.
